baf7ca582d7c7d049ddb99ddca4b943d47747ccc
HB <hbianan@gmail.com>
2019-10-15 17:12:16 -0400

    files updated

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
+const PORT = process.env.PORT || 3000;
 var app = express();
-const PORT = 3000;
 
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('views'))
+
+app.use("/views", express.static(path.join(__dirname, 'views')));
+
 app.set('view engine', 'pug');
 
 app.get('/', (request, response) => {
