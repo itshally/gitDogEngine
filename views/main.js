a4e107297973b8fa2b920c60ad39d866544ec154
HB <hbianan@gmail.com>
2019-10-17 21:43:44 -0400

    files updated
    - added dog breeds list on the home page to display
    - changed from internal scripting to external linking

--- views/main.js
+++ views/main.js
@@ -1,25 +1,58 @@
-// require('axios')
-
 $(document).ready(() => {
 
-     //GET request for all the breeds
+     //Get request for all the list of dog's breed
      $.get('https://dog.ceo/api/breeds/list/all')
       .then((result) => {
-          var dogbreeds = Object.keys(result.message);
-          // console.log(result);
-          for(var x in dogbreeds){
-               var breed = getDogBreed(dogbreeds[x])
-               $('.form-control').append(breed)
+          let dogbreeds = Object.keys(result.message);
+
+          for(let x in dogbreeds){
+               let breed = dogbreeds[x]
+               // console.log(breed)
+               // Displaying all breeds in the home page
+               $('.breeds-container .row').append(
+                    `<div class="col-12 col-md-3 col-lg-2">
+                         <p class="dog-breed">${breed}</p>
+                    </div>`
+               );
+          
+          }
+
+          	//getting the sub-breed
+			  $('.dog-breed').on('click', (e) => {
+				e.preventDefault();
+                    console.log(e.target.textContent)
+                    
+                    // $.get('https://dog.ceo/api/breeds/list/all')
+                    // .then((result) => {
+                    // })
+                    // .catch( (error) => {
+                    //      console.log(error);
+                    // })
+              });
+      })
+      .catch( (error) => {
+          console.log(error)
+      });
+
+
+      $.get('https://dog.ceo/api/breeds/list/all')
+      .then((result) => {
+          let dogbreeds = Object.keys(result.message);
+
+          for(let x in dogbreeds){
+               let breed = getDogBreed(dogbreeds[x])
+               //Displaying all breeds in the gallery's dropdown page
+               $('.form-control').append(breed);
           }
       })
       .catch( (error) => {
           console.log(error)
       });
-      
+
       //displaying the images
-      $('#submit-btn').on('click', (e) => {
-           var x = $('.form-control').val();
-           $('.row').empty();
+     $('#submit-btn').on('click', (e) => {
+          var x = $('.form-control').val();
+          $('.row').empty();
           console.log(x)
           $.get(`https://dog.ceo/api/breed/${x}/images/random/12`)
           .then((result) => {
@@ -27,24 +60,22 @@ $(document).ready(() => {
 
                console.log(result.message)
                for(var x in imgURL){
-                   var img = displayDogImages(imgURL[x]);
+                    var img = displayDogImages(imgURL[x]);
                     $('.row').append(img);
                }
-
-
           })
           .catch((error) => {
                console.log(error)
           });
-     
      });
 
-     // getting the dog breed
+     // getting the dog's breed function
      function getDogBreed(x){
           var output = `<option value='${x}'> ${x} </option>`
-          return output;
+          return output;          
      }
 
+     // displaying each of the dogs image
      function displayDogImages(x){
           var image = `
                <div class='col-12 col-md-4 col-lg-4'>
