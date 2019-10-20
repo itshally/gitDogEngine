fd50fc11eb156588294553d93ac270071908c20d
HB <hbianan@gmail.com>
2019-10-19 20:25:11 -0400

    file updated
    - making a simple function
    - able to determine which breed has a sub-breed

--- views/main.js
+++ views/main.js
@@ -3,40 +3,47 @@ $(document).ready(() => {
      //Get request for all the list of dog's breed
      $.get('https://dog.ceo/api/breeds/list/all')
       .then((result) => {
-          let dogbreeds = Object.keys(result.message);
 
-          for(let x in dogbreeds){
-               let breed = dogbreeds[x]
-               // console.log(breed)
-               // Displaying all breeds in the home page
+          let dogbreeds = Object.keys(result.message);
+          // Displaying all breeds in the home page
+          for(let i in dogbreeds){
                $('.breeds-container .row').append(
                     `<div class="col-12 col-md-3 col-lg-2">
-                         <p class="dog-breed">${breed}</p>
+                         <p class="dog-breed" id="${dogbreeds[i]}">${dogbreeds[i]}</p>
                     </div>`
-               );
-          
+               );     
+
+               if(result.message[dogbreeds[i]].length == 0){
+                    console.log(dogbreeds[i] + " has no sub-degree")
+                    $('#' + dogbreeds[i]).css('color', 'red');
+               }else{
+                    console.log(dogbreeds[i] + ' has a sub-degree')
+               }
           }
 
           	//getting the sub-breed
-			  $('.dog-breed').on('click', (e) => {
-                    e.preventDefault();
+		// 	  $('.dog-breed').on('click', (e) => {
+          //           e.preventDefault();
                     
-                    let breed = e.target.textContent;
-                    console.log(e.target.textContent)
+          //           let breed = e.target.textContent;
+          //           console.log(e.target.textContent)
                     
-                    $.get(`https://dog.ceo/api/breed/${breed}/list`)
-                    .then((result) => {
-                         console.log(result)
+          //           $.get(`https://dog.ceo/api/breed/${breed}/list`)
+          //           .then((result) => {
+          //                console.log(result)
 
-                         if(result.message.length == 0)
-                              console.log('no sub-breed available')
-                         else
-                              console.log('there is available')
-                    })
-                    .catch( (error) => {
-                         console.log(error);
-                    })
-              });
+          //                if(result.message.length == 0){
+          //                     console.log('no sub-breed available')
+          //                }
+          //                else{
+          //                     console.log('there is available')
+          //                }
+                              
+          //           })
+          //           .catch( (error) => {
+          //                console.log(error);
+          //           })
+          //     });
       })
       .catch( (error) => {
           console.log(error)
